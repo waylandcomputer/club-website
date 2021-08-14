20e4ed1aae2ca117ee968be466caeceab184cc84
Kevin-Zhao-Github <Kevin_Zhao@student.wayland.k12.ma.us>
2021-08-13 20:10:06 -0400

    Added signup functionality

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, flash, render_template, send_from_directory
+from flask import flash, Flask, render_template, request, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -25,7 +25,21 @@ def index():
 def get_member_info(member):
     db_result = Member.query.filter_by(fname=member).all()
     if len(db_result) != 1:
-        flash("Member does not exist. Please check that you entered his/her proper first name.")
-        return render_template("index.html")
+        return render_template("all_members.html", members=Member.query.all())
     else:
         return render_template("member.html", member=db_result[0], projects=Project.query.filter_by(owner=db_result[0].id).all())
+
+@app.route("/signup", methods=["GET"])
+def display_signup():
+    return render_template("signup.html")
+
+@app.route("/signup", methods=["POST"])
+def process_signup():
+    fname = request.form["fname"]
+    lname = request.form["lname"]
+    grade = request.form["grade"]
+    print(fname, lname, grade)
+    new_member = Member(fname=fname, lname=lname, grade=grade)
+    db.session.add(new_member)
+    db.session.commit()
+    return render_template("all_members.html", members=Member.query.all())
