6d0999cca382efaf9c0a589a2880ba81ec9128d0
Kevin Zhao <Kevin_Zhao@student.waylandps.org>
2021-09-19 11:55:30 -0400

    Load members from json instead of backend

--- site/src/views/Members.vue
+++ site/src/views/Members.vue
@@ -18,6 +18,7 @@
 
 <script>
     import Member from "../components/Member.vue";
+    import data from "../assets/example4.json";
 
     export default {
         name: "Members",
@@ -31,12 +32,12 @@
         },
         methods: {
             getData() {
-                fetch("http://localhost:5002/data/member_data")
-                    .then((response) => response.json())
-                    .then((data) => {
-                        this.userData = data;
-                        //console.log(data);
-                    });
+                //fetch("http://localhost:5002/data/member_data")
+                //    .then((response) => response.json())
+                //    .then((data) => {
+                //        this.userData = data;
+                //    });
+                this.userData = data;
             },
         },
         created() {
