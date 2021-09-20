f7294d2165cdf2dea1861c60e4c5caa1b72b27f9
boyer <asboyer@gmail.com>
2021-09-20 00:35:56 -0400

    cleaned up animation

--- site/src/views/Members.vue
+++ site/src/views/Members.vue
@@ -48,35 +48,35 @@
         },
     };
 
-document.addEventListener('DOMContentLoaded', function(event){
-    var dataText = ["Current Members"];
+// document.addEventListener('DOMContentLoaded', function(event){
+//     var dataText = ["Current Members"];
 
-    function typeWriter(text, i, fnCallback) {
-        if (i < (text.length)) {
-         document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span id="type" aria-hidden="true"></span>';
+//     function typeWriter(text, i, fnCallback) {
+//         if (i < (text.length)) {
+//          document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span id="type" aria-hidden="true"></span>';
 
-          setTimeout(function() {
-            typeWriter(text, i + 1, fnCallback)
-          }, 150);
-        }
-        else if (typeof fnCallback == 'function') {
-          setTimeout(fnCallback, 700);
-        }
-        }
-        function StartTextAnimation(i) {
-         if (typeof dataText[i] == 'undefined'){
-            setTimeout(function() {
-              StartTextAnimation(0);
-            }, 20000000);
-         }
-        if (i < dataText[i].length) {
-         typeWriter(dataText[i], 0, function(){
-           StartTextAnimation(i + 1);
-         });
-        }
-        }
-        StartTextAnimation(0);
-});
+//           setTimeout(function() {
+//             typeWriter(text, i + 1, fnCallback)
+//           }, 150);
+//         }
+//         else if (typeof fnCallback == 'function') {
+//           setTimeout(fnCallback, 700);
+//         }
+//         }
+//         function StartTextAnimation(i) {
+//          if (typeof dataText[i] == 'undefined'){
+//             setTimeout(function() {
+//               StartTextAnimation(0);
+//             }, 20000000);
+//          }
+//         if (i < dataText[i].length) {
+//          typeWriter(dataText[i], 0, function(){
+//            StartTextAnimation(i + 1);
+//          });
+//         }
+//         }
+//         StartTextAnimation(0);
+// });
 
 
 </script>
