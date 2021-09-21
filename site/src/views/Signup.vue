85f4a6402de695db57523e5cf81e475ce6f70f56
Shen0000 <73674912+Shen0000@users.noreply.github.com>
2021-09-20 21:36:17 -0400

    added css for signup and queue pages
    Co-authored-by: Jayanth-Mani <Jayanth-Mani@users.noreply.github.com>

--- site/src/views/Signup.vue
+++ site/src/views/Signup.vue
@@ -1,16 +1,16 @@
 <template>
-    <div>
+    <div class="signup">
         <form v-on:submit.prevent="submitForm">
+            <label for="fname">First Name </label>
             <div class="form-group">
-                <label for="fname">First Name</label>
                 <input type="text" class="form-control" id="fname" placeholder="John" v-model="form.fname">
             </div>
+            <label for="lname">Last Name </label>
             <div class="form-group">
-                <label for="lname">Last Name</label>
                 <input type="text" class="form-control" id="lname" placeholder="Doe" v-model="form.lname">
             </div>
+            <label for="grade">Grade </label>
             <div class="form-group">
-                <label for="grade">Grade</label>
                 <input type="number" class="form-control" id="grade" placeholder="9" v-model="form.grade">
             </div>
             <input type="submit" value="Submit">
@@ -35,11 +35,17 @@ export default { // code taken from https://5balloons.info/post-form-data-to-api
     },
     methods:{
         submitForm(){
-            axios.post('http://localhost:5002/data/signup', this.form);
-            // clear form
-            this.form.fname = "";
-            this.form.lname = "";
-            this.form.grade = "";
+            if (!this.form.fname || !this.form.lname || !this.form.grade) {
+                alert("Please fill out the fields to submit");
+            }
+            else {
+                axios.post('http://localhost:5002/data/signup', this.form);
+                this.$router.push({ name: 'Queue' });
+                // clear form
+                this.form.fname = "";
+                this.form.lname = "";
+                this.form.grade = "";
+            } 
         }
     }
 }
@@ -47,5 +53,40 @@ export default { // code taken from https://5balloons.info/post-form-data-to-api
 </script>
 
 <style scoped>
+
+.signup {
+    margin: 3.4vw;
+    position: center;
+}
+input {
+    border: #000;
+    text-align: center;
+    font-family: monospace;
+    box-sizing: border-box;
+}
+input[type=text] {
+    width: 50%;
+    padding: 12px 20px;
+    margin: 8px 0;
+}
+
+input[type=number] {
+    width: 50%;
+    padding: 12px 20px;
+    margin: 8px 0;
+}
+
+input[type=submit] {
+    width: 25%;
+    font-size: 3vw;
+    padding: 12px 20px;
+    margin: 8px 0;
+    border-radius: 50px;
+    background-color: #ffbc57;
+}
+input[type=submit]:hover {
+    opacity: 0.7;
+    cursor: pointer;
+}
 
 </style>
