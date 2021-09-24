52d040815bc87bf61170759cdd07f46b8bb17ac4
boyer <asboyer@gmail.com>
2021-09-23 21:21:13 -0400

    added aboyer and default page

---
+++ site/src/views/Default.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="signup">
+        <div class="innerf">
+            <p>
+                This is where each member will create their own unique sub-page on the website!
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+console.log('this is where')
+</script>
+
+<style scoped>
+
+.signup {
+    position: center;
+    background-color: rgb(39, 37, 37);
+    /*padding: 20px;*/
+    width: 100%;
+    height: 100vh;
+
+}
+
+.innerf {
+    border: 5px dashed #FFBC57;
+    margin: 0 auto;
+    padding: 20px;
+    border-radius: 10px;
+    width: 30%;
+    padding: 30px;
+    /*margin-top: 50px;*/
+    /*margin: 0;*/
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    background-color: #1f1d1d;
+}
+
+
+p {
+    /*margin-top: 18px;*/
+    /*margin-bottom: 2px;*/
+    font-size: 15px;
+    text-transform: lowercase;
+}
+
+</style>
